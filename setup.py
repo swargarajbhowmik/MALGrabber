8d0dbad9f5e0951f3979eaccdd535a84fc953210
Swargaraj Bhowmik <swargarajbhowmik@gmail.com>
2023-08-17 11:53:31 +0530

    Added Anime & Manga Search

--- setup.py
+++ setup.py
@@ -2,13 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="malgrabber",
-    version="1.0",
+    version="1.1",
     packages=find_packages(),
-    # entry_points={
-    #     'console_scripts': [
-    #         'mal = mal.cli:info'
-    #     ],
-    # },
     install_requires=[
         "requests",
         "beautifulsoup4",
@@ -17,9 +12,6 @@ setup(
     ],
     author="Swargaraj Bhowmik",
     author_email="contact@swargarajbhowmik.me",
-    description="Simple Python Package for MediaFire File Download and Information Retrieval",
     license="MIT",
     url="https://github.com/swargarajbhowmik/malgrabber",
-    # long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
 )
